3fe2e4de7823d10f7097e93777191e422d5764f8
Hanwool <hanwoollee911@gmail.com>
2023-05-26 01:31:36 -0700

    Test s3 bucket

--- lib/sammo_helper_cdk-stack.ts
+++ lib/sammo_helper_cdk-stack.ts
@@ -1,16 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class SammoHelperCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'SammoHelperCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new Bucket(this, 'MyFirstBucket', {
+      versioned: true
+    });
   }
 }
