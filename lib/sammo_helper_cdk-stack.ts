2097746153b78cf51452e14c981bb235af3f2dc7
Hanwool <hanwoollee911@gmail.com>
2024-02-14 02:51:27 -0800

    Add AppSync to cdk, update importScript

--- lib/sammo_helper_cdk-stack.ts
+++ lib/sammo_helper_cdk-stack.ts
@@ -1,13 +1,34 @@
 import * as cdk from 'aws-cdk-lib';
-import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { AuthorizationType, GraphqlApi, SchemaFile } from 'aws-cdk-lib/aws-appsync';
+import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
+import path = require('path');
 
 export class SammoHelperCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new Bucket(this, 'MyFirstBucket', {
-      versioned: true
+    const api = new GraphqlApi(this, 'SammoHelperAPI', {
+      name: 'General',
+      schema: SchemaFile.fromAsset(path.join(__dirname, 'schema.graphql')),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: AuthorizationType.API_KEY,
+        },
+      },
+      xrayEnabled: true,
+    });
+
+    const generalTable = new Table(this, 'GeneralTable', {
+      tableName: 'General',
+      partitionKey: {
+        name: 'server',
+        type: AttributeType.STRING
+      },
+      sortKey: {
+        name: 'name',
+        type: AttributeType.STRING
+      }
     });
   }
 }
